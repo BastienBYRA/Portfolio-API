34ab825dc7efef00450ebe24296cb8717aff177b
Bastien <neitsabast@gmail.com>
2023-06-12 21:39:39 +0200

    [Add] - Classe et Interface du GenericRepository et UnitOfWork, à completer

---
+++ Services/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿namespace Portfolio_API.Services
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        #region Repository
+
+        #endregion
+
+        int Save();
+    }
+}
---
+++ Services/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using Portfolio_API.Data;
+
+namespace Portfolio_API.Services
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+
+
+
+        public int Save()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+        }
+    }
+}
